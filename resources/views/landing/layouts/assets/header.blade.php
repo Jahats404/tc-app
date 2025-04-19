64d47e11d628a767dfd39b12692d06d1090abec5
dhimsky <dhimasafri1523@gmail.com>
2025-04-19 21:01:18 +0700

    tt

--- resources/views/landing/layouts/assets/header.blade.php
+++ resources/views/landing/layouts/assets/header.blade.php
@@ -15,6 +15,15 @@
                         <!-- Header Social Area -->
                         <div class="header-social-area d-flex align-items-center">
                             <a href="https://www.instagram.com/tersimpancerita/" target="_blank" data-toggle="tooltip" data-placement="bottom" title="Instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            <a href="https://www.tiktok.com/@tersimpancerita" target="_blank" data-toggle="tooltip" data-placement="bottom" title="TikTok">
+                                <svg xmlns="http://www.w3.org/2000/svg"  width="20" height="20" fill="currentColor" viewBox="0 -100 448 512">
+                                    <path d="M448,209.3V128c-23.4,0-46.4-4.6-67.9-13.5c-21.2-8.8-40.2-21.4-56.5-37.7s-28.9-35.3-37.7-56.5C276.9,4.6,272.3,0,265.9,0
+                                    h-66.6c-7.1,0-12.8,5.7-12.8,12.8v306.1c0,19.3-15.7,35-35,35s-35-15.7-35-35s15.7-35,35-35c4.6,0,9,0.9,13,2.4
+                                    c6.8,2.7,14.6-2.2,14.6-9.6V173.8c0-6.4-5-11.8-11.4-12.8c-8.2-1.2-16.6-1.7-25.2-1.7c-35.2,0-68.3,13.7-93.3,38.7
+                                    C21.7,222.9,8,256,8,291.2c0,35.2,13.7,68.3,38.7,93.3c25,25,58.1,38.7,93.3,38.7s68.3-13.7,93.3-38.7c25-25,38.7-58.1,38.7-93.3
+                                    V208.5c25.6,18.1,56.6,27.8,88.3,27.8c7.1,0,12.8-5.7,12.8-12.8V209.3H448z"/>
+                                </svg>
+                            </a>                            
                         </div>
                         <span>
                             <a href="{{ route('login') }}" class="btn login-btn white-btn">Login</a>
